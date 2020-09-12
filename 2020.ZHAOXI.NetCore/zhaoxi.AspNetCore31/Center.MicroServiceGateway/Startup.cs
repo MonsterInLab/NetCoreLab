fda778cc1efd914ba8adac6f6b6e3a68fbf55934
Stone <m18146611430@163.com>
2020-09-12 16:31:59 +0800

    update Ocelot version 13.8.0

--- 2020.ZHAOXI.NetCore/zhaoxi.AspNetCore31/Center.MicroServiceGateway/Startup.cs
+++ 2020.ZHAOXI.NetCore/zhaoxi.AspNetCore31/Center.MicroServiceGateway/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using Ocelot.Provider.Polly;
 
 namespace Center.MicroServiceGateway
 {
@@ -27,7 +29,7 @@ namespace Center.MicroServiceGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot();
+            services.AddOcelot().AddConsul().AddPolly();
            // services.AddControllers();
         }
 
