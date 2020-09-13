ce424f778c110b036c5bd86efc24e3127add9bbd
Stone <m18146611430@163.com>
2020-09-13 20:47:55 +0800

    验证用户 信息

--- 2020.ZHAOXI.NetCore/zhaoxi.AspNetCore31/Center.Web.Authentication/Controllers/AuthenticationController.cs
+++ 2020.ZHAOXI.NetCore/zhaoxi.AspNetCore31/Center.Web.Authentication/Controllers/AuthenticationController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Center.Interface;
 using Center.Web.Authentication.Utility;
+using Data.EFCCore31.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -16,11 +18,15 @@ namespace Center.Web.Authentication.Controllers
     {
         private readonly ILogger<AuthenticationController> _logger;
         private IJWTService _JWTService = null;
+        private readonly IUserService _iUserService;
 
-        public AuthenticationController(ILogger<AuthenticationController> logger, IJWTService jWTService)
+        public AuthenticationController(ILogger<AuthenticationController> logger,
+            IUserService userService,
+            IJWTService jWTService)
         {
             _JWTService = jWTService;
             _logger = logger;
+            _iUserService = userService;
         }
 
         [Route("Login")]
@@ -28,7 +34,8 @@ namespace Center.Web.Authentication.Controllers
         public string Login(string name, string password)
         {
             ///这里肯定是需要去连接数据库做数据校验
-            if ("stone".Equals(name) && "123456".Equals(password))//应该数据库
+            if (ValidateUser(name,password))
+           // if ("stone".Equals(name) && "123456".Equals(password))//应该数据库
             {
                 {
                     //这里可以去数据库做验证
@@ -48,6 +55,19 @@ namespace Center.Web.Authentication.Controllers
                     token = ""
                 });
             }
+        }
+
+        private bool ValidateUser(string userName, string password)
+        {
+            var users = this._iUserService.Query<User>(u => u.Name == userName && u.Password == password).FirstOrDefault();
+            return users != null;
+        }
+
+        [Route("Get")]
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            return new List<int>() { 1, 2, 3, 4, 5, 6 };
         }
     }
 }
