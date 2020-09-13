09c1637dbfcd8dc6c90baadf3f4871f9c9195f30
Stone <m18146611430@163.com>
2020-09-13 21:30:34 +0800

    add auth web 鉴权授权

---
+++ 2020.ZHAOXI.NetCore/zhaoxi.AspNetCore31/Center.Web.AuthDemo/Utility/CustomAuthorizationFilterAttribute.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.Web.AuthDemo.Utility
+{
+    public class CustomAuthorizationFilterAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (context.ActionDescriptor.EndpointMetadata.Any(item => item is AllowAnonymousAttribute))
+            {
+                return;//匿名 不检查
+            }
+
+            string sUser = context.HttpContext.Request.Cookies["CurrentUser"];
+
+            if (sUser == null)
+            {
+                context.Result = new RedirectResult("~/Home/Index");
+            }
+            else
+            {
+                //还应该检查下权限
+                return;
+            }
+
+        }
+    }
+
+    public class CustomAsyncAuthorizationFilter : Attribute, IAsyncAuthorizationFilter
+    {
+        public Task OnAuthorizationAsync(AuthorizationFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
